3b09d53f4e4e08b6c7eaef9c6ebb1d583f70cfbf
Vasu123ramu <Varayogula.Akhila@ibm.com>
2024-06-01 21:23:23 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4.java
@@ -0,0 +1,15 @@
+package Activies;
+
+import org.junit.jupiter.api.Test;
+
+public class Activity4 {
+
+import org.testng.annotations.Test;
+
+    public class DemoTwo {
+        @Test
+        public void TestCase() {
+            System.out.println("I'm in the test case from DemoTwo Class");
+        }
+    }
+}
