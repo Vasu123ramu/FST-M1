03d4ec692fa2cbe25e780c4f6cf43ec5464453e8
Vasu123ramu <Varayogula.Akhila@ibm.com>
2024-07-04 12:46:55 +0530

    Add files via upload

---
+++ TestNG/Activity4.java
@@ -0,0 +1,20 @@
+package Activies;
+
+import org.junit.jupiter.api.Test;
+
+
+public class Activity4
+{
+        @Test
+        public void TestCase()
+        {
+            System.out.println("I'm in the test case from DemoOne Class");
+        }
+
+         @Test
+         public void TestCase2()
+         {
+            System.out.println("I'm in the test case from DemoTwo Class");
+         }
+}
+
---
+++ TestNG/TestNG_Examples.java
@@ -0,0 +1,9 @@
+package Examples;
+
+import org.testng.annotations.BeforeClass;
+
+public class TestNG_Examples {
+
+    @BeforeClass
+    public void setUpforClass(){System.out.println("Before every test method");}
+}
