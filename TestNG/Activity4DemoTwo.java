c5dad0519ae2251b3353aaa12ec9eb5ac2aeb259
Vasu123ramu <Varayogula.Akhila@ibm.com>
2024-07-22 18:40:03 +0530

    Add files via upload

---
+++ TestNG/Activity4DemoTwo.java
@@ -0,0 +1,11 @@
+package Activies;
+
+import org.testng.annotations.Test;
+
+public class Activity4DemoTwo {
+    
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
